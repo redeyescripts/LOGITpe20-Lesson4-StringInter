f1b26a4a3040fbeeb75d0d5f17372ad054252356
Opilane TTHK <opilane@tthk.ee>
2020-09-08 11:49:05 +0300

    first commit

---
+++ StringInter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Console.WriteLine("Whats your name?");
+            string FirstName;
+            string Lastname;
+            Console.WriteLine("Whats is your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            Lastname = Console.ReadLine();
+            //Console.WriteLine("Hello, " + FirstName + " " + Lastname);
+            Console.WriteLine($"Hello, {FirstName} {Lastname}");
+            
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string FirstName;
+            string Lastname;
+            Console.WriteLine("Whats is your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            Lastname = Console.ReadLine();
+          
+            Console.WriteLine($"Hello, {FirstName} {Lastname}");
+            Console.WriteLine("How old are you?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int Year;
+            Year = Int32.Parse(UserInput);
+            int Age = 2020 - Year;
+            Console.WriteLine("You are born in " + Age );
+        }
+    }
+}
